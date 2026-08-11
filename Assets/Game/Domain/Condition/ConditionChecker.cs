cda789b8fe03a701ce9a1c0802b35b2a152e9313
duktofn <duktofn@gmail.com>
2026-08-11 22:15:55 +0700

    refactor(Game.Domain.Condition/ConditionChecker.cs): refactor ConditionChecker to reduce dependency

--- Assets/Game/Domain/Condition/ConditionChecker.cs
+++ Assets/Game/Domain/Condition/ConditionChecker.cs
@@ -6,38 +6,22 @@ using UnityEngine.Analytics;
 
 namespace Game.Domain.Condition
 {
-    public class ConditionEvaluator
+    public class ConditionChecker
     {
-        private Grid<CellRuntimeData> _grid;
-        
-        public bool Check(Vector2Int CellPos, ConditionRuntimeData condition)
+        public bool Check(List<CellRuntimeData> adjacent, ConditionRuntimeData condition)
         {
-            List <Vector2Int> adjacent = new();
-
-            if (CellPos.y < _grid.GridSize.y) 
-                adjacent.Add(new Vector2Int(CellPos.x, CellPos.y + 1));
-            
-            if (CellPos.y > 0) 
-                adjacent.Add(new Vector2Int(CellPos.x, CellPos.y - 1));
-
-            if (CellPos.x < _grid.GridSize.y)
-                adjacent.Add(new Vector2Int(CellPos.x + 1, CellPos.y));
-            
-            if (CellPos.x > 0) 
-                adjacent.Add(new Vector2Int(CellPos.x - 1, CellPos.y));
-
             foreach (var a in adjacent) 
             {
                 if (condition.Target == ConditionTarget.Food && 
                     condition.Type == ConditionType.Hate &&
-                    condition.FoodTarget == _grid.Get(a.x, a.y).Food)
+                    condition.FoodTarget == a.Food)
                 {
                     return false;
                 }
 
                 if (condition.Target == ConditionTarget.Person && 
                     condition.Type == ConditionType.Hate &&
-                    condition.TargetTrait == _grid.Get(a.x, a.y).Person.Trait)
+                    condition.TargetTrait == a.Person.Trait)
                 {
                     return false;
                 }
