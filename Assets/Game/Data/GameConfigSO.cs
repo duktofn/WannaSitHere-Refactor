136611fbc4ee809ac0b07d6846debe578e87cb2e
duktofn <duktofn@gmail.com>
2026-08-11 11:30:57 +0700

    refactor(Game.Data): change GameConfig's field from static to const

--- Assets/Game/Data/GameConfigSO.cs
+++ Assets/Game/Data/GameConfigSO.cs
@@ -2,11 +2,9 @@ using UnityEngine;
 
 namespace Game.Data
 {
-    [CreateAssetMenu(fileName = "New Game Config", menuName = "Game/New Game Config")]
     public static class GameConfig
     {
-        public static int MAX_CONDITION_PER_PERSON = 2;
-        public static int MORE_MOVE_AMOUNT = 3;
-        
+        public const int MAX_CONDITION_PER_PERSON = 2;
+        public const int MORE_MOVE_AMOUNT = 3;
     }
 }
