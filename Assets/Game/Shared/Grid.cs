091073a52cae8deadbd07d42d499fb002727210b
duktofn <duktofn@gmail.com>
2026-08-12 18:39:44 +0700

    refactor(Game.Shared/Grid.cs): add constructors + fix boundary check

--- Assets/Game/Shared/Grid.cs
+++ Assets/Game/Shared/Grid.cs
@@ -10,21 +10,61 @@ namespace Game.Shared
         public Vector2 CellSize { get; }
         public Vector2 CellDistance { get; }
 
-        [SerializeField,Range(0,1)] private float PosX;
-        [SerializeField,Range(0,1)] private float PosY;
+        [SerializeField, Range(0, 1)] private float PosX;
+        [SerializeField, Range(0, 1)] private float PosY;
 
         public T[,] GridContent { get; }
 
+        public Grid(
+            Vector2Int gridSize,
+            Vector2 cellSize,
+            Vector2 cellDistance,
+            float posX,
+            float posY)
+        {
+            if (gridSize.x <= 0 || gridSize.y <= 0)
+                throw new ArgumentOutOfRangeException(nameof(gridSize));
+
+            GridSize = gridSize;
+            CellSize = cellSize;
+            CellDistance = cellDistance;
+
+            PosX = Mathf.Clamp01(posX);
+            PosY = Mathf.Clamp01(posY);
+
+            GridContent = new T[gridSize.x, gridSize.y];
+        }
+
+        public Grid(Grid<T> other)
+        {
+            GridSize = other.GridSize;
+            CellSize = other.CellSize;
+            CellDistance = other.CellDistance;
+            PosX = other.PosX;
+            PosY = other.PosY;
+            GridContent = other.GridContent;
+        }
+
+        public Grid(Vector2Int size)
+        {
+            GridSize = size;
+            GridContent = new T[size.x, size.y];
+        }
+
         public T Get(int x, int y)
         {
-            if (x > GridSize.x || y > GridSize.y || x < 0 || y < 0) return default;
-            return GridContent[x,y];
+            if (x >= GridSize.x || y >= GridSize.y || x < 0 || y < 0)
+                return default;
+
+            return GridContent[x, y];
         }
 
         public void Set(int x, int y, T value)
         {
-            if (x > GridSize.x || y > GridSize.y || x < 0 || y < 0) return;
-            GridContent[x,y] = value;
+            if (x >= GridSize.x || y >= GridSize.y || x < 0 || y < 0)
+                return;
+
+            GridContent[x, y] = value;
         }
     }
 }
