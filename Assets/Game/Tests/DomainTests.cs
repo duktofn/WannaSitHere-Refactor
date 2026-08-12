b3367f9bac868af7c4ef86937fbc14004b9e66a3
duktofn <duktofn@gmail.com>
2026-08-12 22:28:23 +0700

    test: add DomainTests NUnit unit test suite and LevelTest scene component

---
+++ Assets/Game/Tests/DomainTests.cs
@@ -0,0 +1,91 @@
+using NUnit.Framework;
+using UnityEngine;
+using Game.Shared;
+using Game.Domain.Grid;
+using Game.Domain.Person;
+using Game.Domain.Condition;
+using Game.Domain.SaveAndLoad;
+using System.Collections.Generic;
+
+namespace Game.Tests
+{
+    public class DomainTests
+    {
+        [Test]
+        public void Grid_GetAndSet_WorksCorrectlyWithinBounds()
+        {
+            var grid = new Grid<int>(new Vector2Int(3, 3));
+            grid.Set(1, 1, 42);
+
+            Assert.AreEqual(42, grid.Get(1, 1));
+            Assert.AreEqual(0, grid.Get(0, 0));
+        }
+
+        [Test]
+        public void Grid_OutOfBounds_ReturnsDefaultAndDoesNotThrow()
+        {
+            var grid = new Grid<int>(new Vector2Int(2, 2));
+
+            Assert.AreEqual(0, grid.Get(-1, 0));
+            Assert.AreEqual(0, grid.Get(2, 2));
+
+            Assert.DoesNotThrow(() => grid.Set(-1, 0, 10));
+            Assert.DoesNotThrow(() => grid.Set(5, 5, 10));
+        }
+
+        [Test]
+        public void PersonRuntimeData_SetState_FiresOnPersonStateChangedEvent()
+        {
+            var person = new PersonRuntimeData("TestPerson", PersonTrait.Cool, new List<ConditionRuntimeData>(), null);
+            PersonState receivedState = PersonState.Normal;
+            bool eventFired = false;
+
+            person.OnPersonStateChanged += (state) =>
+            {
+                eventFired = true;
+                receivedState = state;
+            };
+
+            person.SetState(PersonState.Happy);
+
+            Assert.IsTrue(eventFired);
+            Assert.AreEqual(PersonState.Happy, receivedState);
+            Assert.AreEqual(PersonState.Happy, person.State);
+        }
+
+        [Test]
+        public void ConditionChecker_HateFoodCondition_ReturnsFalseWhenAdjacentMatches()
+        {
+            var checker = new ConditionChecker();
+            var condition = new ConditionRuntimeData(
+                ConditionType.Hate,
+                ConditionTarget.Food,
+                PersonTrait.Cool,
+                Food.Hamburger,
+                "Hates Hamburger",
+                "Angry at Hamburger"
+            );
+
+            var cellWithFood = new CellRuntimeData(Vector2.zero, CellType.Food, null, Food.Hamburger, null);
+            var adjacentCells = new List<CellRuntimeData> { cellWithFood };
+
+            bool result = checker.Check(adjacentCells, condition);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void LevelRuntimeData_ModifyMove_UpdatesLevelMoveCount()
+        {
+            var mainGrid = new Grid<CellRuntimeData>(new Vector2Int(2, 2));
+            var waitGrid = new Grid<CellRuntimeData>(new Vector2Int(2, 1));
+            var level = new LevelRuntimeData(10, mainGrid, waitGrid);
+
+            level.ModifyMove(-1);
+            Assert.AreEqual(9, level.LevelMove);
+
+            level.ModifyMove(3);
+            Assert.AreEqual(12, level.LevelMove);
+        }
+    }
+}
