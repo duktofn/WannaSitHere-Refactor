ace2c6f880737f214d1292a64c873dc46379d15a
duktofn <duktofn@gmail.com>
2026-09-11 10:21:10 +0700

    feat(Game.View.UI): support single-press behavior in ButtonEventRaiser

--- Assets/Game/View/UI/ButtonEventRaiser.cs
+++ Assets/Game/View/UI/ButtonEventRaiser.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Game.Events;
 using Cysharp.Threading.Tasks;
+using UnityEngine.UI;
 
 namespace Game.View.UI
 {
@@ -13,8 +14,26 @@ namespace Game.View.UI
         [Header("Timing")]
         [SerializeField] private float delay;
 
+        [Header("Button Setting")]
+        [SerializeField] private bool isPressedOnce;
+        [SerializeField] private Button button;
+
+        private void Awake()
+        {
+            button = GetComponent<Button>();
+            button.interactable = true;
+            var colors = button.colors;
+            colors.disabledColor = colors.normalColor; 
+            button.colors = colors;
+        }
+
         public void RaiseAll()
         {
+            if (isPressedOnce)
+            {
+                button.interactable = false;
+            }
+
             if (delay > 0f)
             {
                 RaiseAllWithDelay().Forget();
